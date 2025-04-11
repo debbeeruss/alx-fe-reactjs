81e0e9bbfdcf714668078dfe92ef5826cff9d7b7
DebbieRuss <debbyrusselqueen111@gmail.com>
2025-04-11 08:39:59 +0100

    Search.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import UserSearch from './components/UserSearch';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="min-h-screen p-6 bg-gray-100">
       <h1 className="text-2xl font-bold text-center mb-6">GitHub User Search</h1>
       <UserSearch />
+      <Search />
+    </div>
     </div>
   );
 }
